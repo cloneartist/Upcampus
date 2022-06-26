78b60dbcc104fadda03afaab361484d316af2719
hr4ujohns1995 <hr4ujohns@gmail.com>
2022-06-26 13:04:45 +0530

    project1 completed

--- lib/screens/completed.dart
+++ lib/screens/completed.dart
@@ -24,6 +24,15 @@ class _ProjectCompletedState extends State<ProjectCompleted> {
                 fit: BoxFit.contain,
               ),
             ),
+            Text(
+              "Project 1",
+              style: GoogleFonts.poppins(
+                  fontStyle: FontStyle.normal,
+                  fontSize: 18,
+                  color: Color.fromARGB(255, 97, 103, 128),
+                  textStyle: TextStyle(height: 3)),
+            ),
+
             Padding(
               padding: const EdgeInsets.fromLTRB(50, 5, 100, 10),
               child: Text(
@@ -33,12 +42,61 @@ class _ProjectCompletedState extends State<ProjectCompleted> {
                   fontSize: 14,
                 ),)),
 
-
-
-            Text(
-              "data,",
-              style: GoogleFonts.poppins(),
+                    Padding(
+              padding: const EdgeInsets.fromLTRB(50, 20, 100, 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "How to?",
+                  style: GoogleFonts.poppins(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color.fromARGB(255, 97, 103, 128)),
+                      
+                ),
+              ),
             ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 10, 250, 0),
+              child: Text(
+                  "•\tYoutube. \n•\tW3Schools.\n",
+                  style: TextStyle(height: 1.3),
+              )
+            ),
+              Padding(
+              padding: const EdgeInsets.fromLTRB(50,2, 100, 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Completed?",
+                  style: GoogleFonts.poppins(
+                      fontStyle: FontStyle.normal,
+                      fontSize: 16,
+                      color: Color.fromARGB(255, 97, 103, 128)),
+                      
+                ),
+              ),
+            ),
+             Padding(
+              padding: const EdgeInsets.fromLTRB(50,10, 270, 2),
+              child: Text(
+                "\nmail to us",
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(30, 5, 150, 20),
+              child: Text(
+                "submission@upschool.in",
+                style: GoogleFonts.poppins(
+                  
+                  fontSize: 14,
+                ),)),
+
+
             Padding(
               padding: const EdgeInsets.fromLTRB(15, 8, 15, 8),
               child: ElevatedButton(
